f16caf6ed2ca7f5389a669059376a545d599e470
vansh-000 <23165@iiitu.ac.in>
2025-03-26 00:44:23 +0530

    feat: add Problem model and routes for problem management

---
+++ src/models/problems.model.js
@@ -0,0 +1,46 @@
+import mongoose from "mongoose";
+
+const ProblemSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    trim: true,
+    unique: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  difficulty: {
+    type: String,
+    required: true,
+    enum: ["Easy", "Medium", "Hard"],
+  },
+  likes: {
+    type: Number,
+    default: 0,
+    min: 0,
+  },
+  dislikes: {
+    type: Number,
+    default: 0,
+    min: 0,
+  },
+  videoId: {
+    type: String,
+    required: true,
+  },
+  link: {
+    type: String,
+    required: true,
+    validate: {
+      validator: function (v) {
+        return /^https?:\/\/.+\..+/.test(v);
+      },
+      message: "Invalid URL format",
+    },
+  },
+});
+
+const Problem = mongoose.model("Problem", ProblemSchema);
+export default Problem;
